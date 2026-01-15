32b21d67e320db17547d5270462ce1d1e5e37ae1
eliora7 <elioral770@gmail.com>
2026-01-15 14:52:09 +0200

    סיום עיצוב דף הפרויקט (נשאר פירוט על כל מפתחת לומדה בINFO)

--- src/PartSix/Project.jsx
+++ src/PartSix/Project.jsx
@@ -6,10 +6,19 @@ function Project({ user }) {
   const navigate = useNavigate();
 
   return (
-    <div className="Project">
+    <div className="Project"
+      style={{
+          backgroundImage: `url(${process.env.PUBLIC_URL}/Assets/PinkBackground.jpg)`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat'
+      }}
+    >
         <h1 className='Dev-Title'>בשעה טובה!</h1>
         <p className='Dev-Title2'>סיימת את הלומדה בהצלחה! הגיע הזמן קצת לתרגל את כל מה שלמדת ולהתנסות בעצמך. תיעזרי בנו בכל מה שאת צריכה, אנחנו תמיד כאן בשבילך, בשיהיה בהצלחה ❤️</p>
-
+        <p className='Dev-Text'> בלחיצה על הכפתור "לפרויקט החפיפה" יפתח לך הדרייב עם התיקייה הנכונה, בתוך התיקייה יש מסמך דרישות הלקוח, סטוריבורד וחומר מקצועי. עליך לעבור ולקרוא את כל המסמכים ואז למלא את הסטוריבורד, אחרי שתסיימי תקראי לי לעזרה והמלצות ואז תתחילי לעצב את הלומדה. תחילה תעצבי את כל הלומדה למסך מחשב ולמובייל בפיגמה ואז תעברי לvscode.</p>
+        <h2 className='Dev-Title3'>בהצלחה!</h2>
+        <button className='projectBtn' onClick={() => window.open('https://drive.google.com/drive/folders/1df18CiMLS1aTDhqTLSTktCn5dcySRO-Z?usp=drive_link', '_blank')}>לפרויקט החפיפה</button>
     </div>
   );
 }
